027b6e87c0666269094f3713e8ca5b92a066ae0e
veerendrareddyv <veerendrareddyv@gmail.com>
2018-08-05 23:42:47 -0300

    project is upload

---
+++ src/test/java/com/veerendra/testng/examples/loadtest/TestMultiples.java
@@ -0,0 +1,47 @@
+package com.veerendra.testng.examples.loadtest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class TestMultiples {
+
+	  private WebDriver driver;
+
+	
+	  @BeforeClass
+	  public void beforeClass() {
+		  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Matias\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	      driver = new ChromeDriver();
+	  }
+
+	  @AfterClass
+	  public void afterClass() {
+	      driver.quit();
+	  }
+
+	@Test()
+	public void loadTestGoogle() {
+
+		driver.get("http://www.google.com");
+		System.out.println("Page Title is " + driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
+
+	}
+	
+	@Test(dependsOnMethods = { "loadTestGoogle" })	
+	public void loadTestNorthernTrust() {
+
+		driver.get("http://www.ntrs.com");
+		System.out.println("Page Title is " + driver.getTitle());
+		Assert.assertEquals("Northern Trust | Wealth Management, Asset Management & Asset Servicing", driver.getTitle());
+
+	}
+
+	
+
+}
---
+++ src/test/resources/snippet/Snippet.java
@@ -0,0 +1,7 @@
+package snippet;
+
+public class Snippet {
+	java -cp C:\Users\Lenovo\Desktop\eclipse\plugins\org.testng.eclipse_6.9.12.201607091356\lib\*;C:\Test org.testng.TestNG testng.xml
+	
+}
+
